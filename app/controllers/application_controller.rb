84207481ed07175feeff22ac965df5654cda830d
Eric Rumble <eric.rumble@sage.com>
2017-03-05 19:45:53 -0500

    add some comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
   include ExceptionHandler
+
+  # --------------------------------------------------------------------------------------------
+  # Uncomment the following method to change Knock's default behavior of returning :unauthorized
+  # on requiests with invalid, or missing, tokens
+  # --------------------------------------------------------------------------------------------
+  # def unauthorized_entity(entity_name)
+  #   head :not_found
+  # end
 end
